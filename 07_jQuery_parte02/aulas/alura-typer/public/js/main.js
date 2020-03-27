32713cfc9f1159b5569abc66018893341ee80da7
IvanConsalter <ivanconsalter1@gmail.com>
2020-03-27 15:17:16 -0300

    exibir e ocultar elementos com show(), hide(), toggle(), slideDown(), slideUp(), slideToggle()

--- 07_jQuery_parte02/aulas/alura-typer/public/js/main.js
+++ 07_jQuery_parte02/aulas/alura-typer/public/js/main.js
@@ -97,44 +97,7 @@ function inicializaMarcadores(){
 	});
 }
 
-function novaLinha(usuario, numPalavras){
 
-	var linha = $('<tr>');
-	var colunaUsuario = $('<td>').text(usuario);
-	var colunaPalavras = $('<td>').text(numPalavras);
-	var colunaRemover = $('<td>');
-
-	var link = $('<a>').attr('href', '#').addClass('botao-remover');
-	var icone = $('<i>').addClass('small').addClass('material-icons').text('delete');
-
-	linha.append(colunaUsuario);
-	linha.append(colunaPalavras);
-
-	link.append(icone);
-	colunaRemover.append(link);
-	linha.append(colunaRemover);
-
-	return linha;
-
-}
-
-function removeLinha(event){
-
-	event.preventDefault();
-	$(this).parent().parent().remove();
-}
-
-function inserePlacar(){
-
-	var corpoTabela = $('#placar').find('tbody');
-	var usuario = 'nome';
-	var numPalavras = $('#contador-palavras').text();
-
-	var linha = novaLinha(usuario, numPalavras);
-
-	linha.find('.botao-remover').click(removeLinha);
-	corpoTabela.append(linha);
-}
 
 
 
